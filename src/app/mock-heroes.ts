8e5e763a0175840b981476ba22b45cb59878ccb5
galataea <michael.gent1@gmail.com>
2018-05-07 01:43:02 -0600

    'Updates'

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Magneto'},
+    { id: 12, name: 'Wolverine'},
+    { id: 13, name: 'Thanos'},
+    { id: 14, name: 'Apocalypse'},
+    { id: 15, name: 'Thor'},
+    { id: 16, name: 'Hulk'},
+    { id: 17, name: 'Spiderman'},
+    { id: 18, name: 'Cyclopse'},
+    { id: 19, name: 'Beast'},
+    { id: 20, name: 'Psylock'}
+    
+];
